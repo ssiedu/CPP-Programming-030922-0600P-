ec06b80815e04f48e007b46b66abaf31947c43ba
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-01-16 19:58:56 +0530

    program

---
+++ ternary.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"\n Enter any number : ";
+	cin>>number;
+	(number>0)?cout<<"Number positive" : cout<<"Number negative";
+	return 0;
+}
